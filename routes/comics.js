cec7a7e23dc7ae75eeb281a790bc555ac61088cf
Olivier Désiré <olivierdesire@MacBook-Pro-de-Olivier.local>
2023-02-18 14:41:59 +0100

    Ajout route comic

--- routes/comics.js
+++ routes/comics.js
@@ -32,4 +32,15 @@ router.get("/comics/:characterId", async (req, res) => {
   }
 });
 
+router.get("/comic/:comicId", async (req, res) => {
+  try {
+    const character = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/character/${req.params.comicId}?apiKey=${process.env.APIKEY}`
+    );
+    return res.status(200).json(character.data);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+});
+
 module.exports = router;
